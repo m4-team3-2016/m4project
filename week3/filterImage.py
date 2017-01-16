d25658af5a4cff2ede2779cdf0058e6a9e08d954
alejandronespereira <alejandronespereira@gmail.com>
2017-01-16 01:24:40 +0100

    Last commit of filtering by area

--- week3/filterImage.py
+++ week3/filterImage.py
@@ -6,15 +6,19 @@ def filterImage(im,P):
     if isinstance(im,str):
         im = cv2.imread(im,0)
 
-    if im.shape[2] = 3:
-        im = im[:,:,0]
+    if len(im.shape) == 3:
+        if im.shape[2] == 3:
+            im = im[:,:,0]
 
-    out,contours,hierarchy = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    filteredImage = np.ones((im.shape[0],im.shape[1],3),dtype = np.uint8)
 
-    for c in contours:
+    out,contours,hierarchy = cv2.findContours(np.array(im), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+    for idx,c in enumerate(contours):
         area = cv2.contourArea(c)
         if area <= P:
-            unPaintThePixels = True
-        else
+            out = cv2.drawContours(filteredImage,contours,idx,(0,0,0),-1)
+
+    im = np.bitwise_and(out[:,:,0],im)
 
     return im
