d6d51e099c63ece6518d6834945795553d78845b
marccarne <marc.carne.herrera@gmail.com>
2017-01-30 21:31:03 +0100

    Update evaluateOpticalFlow.py

--- week4/evaluateOpticalFlow.py
+++ week4/evaluateOpticalFlow.py
@@ -52,6 +52,51 @@ def msen(resultOF, gtOF):
     return errorArray, error, imageToReconstruct
 
 
+def msen_no_peta(resultOF, gtOF):
+    errorVector = []
+    correctPrediction = []
+
+    uResult = []
+    vResult = []
+    uGT = []
+    vGT = []
+    imageToReconstruct = []
+
+    validGroundTruth = []
+
+    # flow_u(u, v) = ((float)I(u, v, 1) - 2 ^ 15) / 64.0;
+    # flow_v(u, v) = ((float) I(u, v, 2) - 2 ^ 15) / 64.0;
+    # valid(u, v) = (bool)I(u, v, 3);
+    for pixel in range(0,resultOF[:,:,0].size):
+        uResult.append( ((float)(resultOF[:,:,0].flat[pixel]) - math.pow(2, 15) ) / 64.0 )
+        vResult.append(((float)(resultOF[:,:,1].flat[pixel])-math.pow(2, 15))/64.0)
+        uGT.append(((float)(gtOF[:,:,0].flat[pixel])-math.pow(2, 15))/64.0)
+        vGT.append(((float)(gtOF[:,:,1].flat[pixel])-math.pow(2, 15))/64.0)
+        #validGroundTruth.append( gtOF[:,:,0].flat[pixel] )
+
+    for idx in range(len(uResult)):
+        #if validGroundTruth[idx] == 0:
+        #    imageToReconstruct.append(0)
+        #    continue
+        #else:
+        squareError = math.sqrt(math.pow((uGT[idx] - uResult[idx]), 2) + math.pow((vGT[idx] - vResult[idx]), 2))
+
+        errorVector.append(squareError)
+        imageToReconstruct.append(squareError)
+
+        if (squareError > 3):
+            correctPrediction.append(0)
+        else:
+            correctPrediction.append(1)
+
+    error = (1 - sum(correctPrediction)/(float)(len(uResult))) * 100;
+
+    errorArray = np.asarray(errorVector)
+
+    return errorArray, error, imageToReconstruct
+
+
+
 def transformGTOF(OFimage):
     r, c, d = OFimage.shape;
     OFimage = block_reduce(OFimage, block_size=(2,2,1), func=np.mean)
@@ -112,7 +157,7 @@ if __name__ == "__main__":
     print(gt_matrix.shape)
 
     # Compute metrics
-    msenValues, error, image = msen(motion_matrix, gt_matrix)
+    msenValues, error, image = msen_no_peta(motion_matrix, gt_matrix)
 
     print (msenValues)
     print (error)
