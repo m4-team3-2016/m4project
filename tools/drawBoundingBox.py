cf0e58b75970a273435d2550d8114af674c3c9b5
alejandronespereira <alejandronespereira@gmail.com>
2017-02-05 18:27:10 +0100

    drawing fixes

--- tools/drawBoundingBox.py
+++ tools/drawBoundingBox.py
@@ -13,12 +13,17 @@ def drawBBoxWithText(image,text,topLeft,bottomRight,color,alpha):
 
     cv2.addWeighted(frame, alpha, image, 1 - alpha, 0, image)
 
-    cv2.putText(image,text,(topLeft[0]+5,bottomRight[1]+textDisplacementX),font,fontScale,(255,255,255))
-    return image
-if __name__ == "__main__":
-    im = np.ones((600,480,3))
-    text = "d"
-    text = "id:0 - 72kmh"
-    a = drawBBoxWithText(im,text,(200,200),(350,350),(0,255,255),0.5)
+    cv2.putText(frame,text,(topLeft[0]+5,bottomRight[1]+textDisplacementX),font,fontScale,(255,255,255))
+    return frame
+def drawCentroidWithText(image,text,centroid,color,alpha):
+    textDisplacementX = 10
+    font = cv2.FONT_HERSHEY_COMPLEX
+    fontScale = 0.4
+    textSize = cv2.getTextSize(text, font, fontScale,1)
+    frame = np.ones_like(image)
+    cv2.circle(frame,centroid,3,color,-1)
 
-    cv2.imwrite('./test.png',a)
+    cv2.rectangle(frame,(centroid[0]+15,centroid[1]),(centroid[0] + 15 + textDisplacementX + textSize[0][0] ,centroid[1] + 15),color,-1)
+    cv2.putText(frame,text,(centroid[0]+20,centroid[1]+textDisplacementX),font,fontScale,(255,255,255))
+
+    return frame
