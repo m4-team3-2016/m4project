9d017cfe80315c7fd523e319638bc5ab12bb3a08
alejandronespereira <alejandronespereira@gmail.com>
2017-02-05 20:07:59 +0100

    Data reader update

--- week5/dataReader.py
+++ week5/dataReader.py
@@ -2,24 +2,22 @@ import cv2
 import week5configuration as finalConf
 import numpy as np
 
-def getSingleFrame(dataFile,frameNumber):
+def getSingleFrame(dataFile,frameNumber,convertColor = True):
     if isinstance(dataFile,list):
         frame =  cv2.imread(list[frameNumber])
     else:
         dataFile.set(1,frameNumber)
         ret,frame = dataFile.read()
-        cv2.imshow('tests',frame)
-        cv2.waitKey(0)
-        print "data read from video"
-    print frame.shape
-    if finalConf.colorSpace != "BGR":
-        print "Changing colorspace"
-        print type(frame)
-        frame = cv2.cvtColor(frame,6) #finalConf.colorSpaceConversion[finalConf.colorSpace])
+    print convertColor
+    if finalConf.colorSpace != "BGR" and convertColor:
+        print "Converting"
+        frame = cv2.cvtColor(np.asarray(frame),finalConf.colorSpaceConversion[finalConf.colorSpace])
+    else:
+        print"not converting"
     return frame
 
-def getFrameAndPrevious(dataFile,frameNumber):
-    return getSingleFrame(dataFile,frameNumber),getSingleFrame(dataFile,frameNumber-1)
+def getFrameAndPrevious(dataFile,frameNumber,convertColor = True):
+    return getSingleFrame(dataFile,frameNumber,convertColor),getSingleFrame(dataFile,frameNumber-1,convertColor)
 
-def getFrameAndNext(dataFile,frameNumber):
-    return getSingleFrame(dataFile,frameNumber),getSingleFrame(dataFile,frameNumber+1)
+def getFrameAndNext(dataFile,frameNumber,convertColor = True):
+    return getSingleFrame(dataFile,frameNumber,convertColor),getSingleFrame(dataFile,frameNumber+1,convertColor)
